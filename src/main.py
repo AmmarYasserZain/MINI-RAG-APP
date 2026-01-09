977ce3cc24e69bdbdcd3c3ec3e1b0fc4a993c928
AmmarYasser <ammaryaser3301@gmail.com>
2026-01-09 23:13:46 +0200

    create mongo schemes and models

--- src/main.py
+++ src/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
 from routes import base, data
+from contextlib import asynccontextmanager
+from helpers.config import Settings, get_settings
+from motor.motor_asyncio import AsyncIOMotorClient
 
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    settings: Settings = get_settings()
+    
+    # Startup Logic
+    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
+    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+
+    yield  # The app runs while it stays here
+    
+    # Shutdown logic
+    app.mongo_conn.close()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
