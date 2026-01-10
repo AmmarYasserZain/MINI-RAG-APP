2b361938a387c4e90a4d02d04526be473b8c96cb
AmmarYasser <ammaryaser3301@gmail.com>
2026-01-11 01:36:26 +0200

    create asset collection

--- src/routes/data.py
+++ src/routes/data.py
@@ -5,8 +5,10 @@ from models import ResponseSignal
 from .schemes.data import ProcessRequest
 from fastapi.responses import JSONResponse
 from models.ChunkModel import ChunkModel
+from models.AssetModel import AssetModel
 from models.ProjectModel import ProjectModel
-from models.db_schemes import Project, DataChunk
+from models.enums.AssetTypeEnum import AssetTypeEnum
+from models.db_schemes import Project, DataChunk, Asset
 from helpers.config import Settings, get_settings
 from controllers import DataController, ProjectController, ProcessController
 from fastapi import FastAPI, APIRouter, UploadFile, status, Request, Depends
@@ -66,10 +68,24 @@ async def upload_data(request: Request, project_id: str, file: UploadFile,
             }
         )
     
+    
+    # Store the assets into the database
+    asset_model = await AssetModel.create_instance(
+        db_client=request.app.db_client
+    )
+
+    asset_resource = Asset(
+        asset_project_id=project.id,
+        asset_type=AssetTypeEnum.FILE.value,
+        asset_name=file_id,
+        asset_size=os.path.getsize(file_path)
+    )
+    asset_record = await asset_model.create_asset(asset=asset_resource)
+
     return JSONResponse(
         content={
             "signal" : ResponseSignal.FILE_UPLOAD_SUCCESS.value,
-            "file_id": file_id,
+            "file_id": str(asset_record.id),
         }
     )
 
